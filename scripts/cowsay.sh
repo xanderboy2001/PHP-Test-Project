9d1eb697598dd7a9883a63baa9ed755061c01fbb
Alex Christian <alexanderechristian@gmail.com>
2025-12-11 16:49:06 -0500

    Added cowsay script to test input

---
+++ scripts/cowsay.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+set -o pipefail
+set -u
+[[ -n "${DEBUG:-}" ]] && set -x
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
+__base="$(basename "${__file}" .sh)"
+
+
+cowsay $1
