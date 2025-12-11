b15bdeb7392575275531167d88da42ace2742c7c
Alex Christian <alexanderechristian@gmail.com>
2025-12-11 10:00:17 -0500

    prints system info with landscape-sysinfo

---
+++ scripts/sysinfo.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+set -o pipefail
+set -u
+[[ -n "${DEBUG:-}" ]] && set -x
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
+__base="$(basename "${__file}" .sh)"
+
+landscape-sysinfo
